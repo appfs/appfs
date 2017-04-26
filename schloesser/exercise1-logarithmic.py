b06d53505f06559cd44792a5eb73e2dae51887f3
Franziska Schlösser <schloesser@zib.de>
2017-04-26 17:22:26 +0200

    julia version and comments into main python version

---
+++ schloesser/exercise1-logarithmic.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import sys
+import math
+
+filename = sys.argv[1]
+
+with open(filename, "r") as f:
+	valid = [0,0,0]
+	mean = [0,0]
+	for i, line in enumerate(f):
+	    tmp = line.split("\n")[0].split("#")[0].split("; ")
+	    if len(tmp) < 3:
+	        continue
+	    try:
+	        pos = int(tmp[1])
+	        val = float(tmp[2])
+	        if (pos == 1 or pos == 2) and val > 0:
+	            mean[pos-1] = mean[pos-1] + math.log(val)
+	            valid[pos] = valid[pos]+1
+	    except ValueError:
+	        continue
+	    valid[0] = valid[0]+1
+
+print("File: " + str(filename) + " with " + str(valid[0]) + " lines")
+for i in range(2):
+    mean[i] = math.exp(mean[i]/valid[i+1])
+    print("Valid values Loc" + str(i) + ": " + str(valid[i+1]) + " with GeoMean: " + str(mean[i]))
+
--- schloesser/exercise1.py
+++ schloesser/exercise1.py
@@ -4,31 +4,44 @@ import numpy as np
 import scipy, scipy.stats
 import sys
 
+# read filename from commandlineargs
 filename = sys.argv[1]
 
-f = open(filename, "r")
-valid = [0,0,0]
-loc = [[],[]]
-mean = []
-for i, line in enumerate(f):
-    tmp = line.split("\n")[0].split("#")[0].split("; ")
-    if len(tmp) < 3:
-        continue
-    if tmp[1] != "" and tmp[2] != "":
+# open file
+with open(filename, "r") as f:
+    # store [number of correct lines, number of positions in location1, number of positions in location2]
+    valid = [0,0,0]
+    # store values in loc1, loc2 respectively
+    loc = [[],[]]
+    # store the geometric mean of both locations
+    mean = []
+
+	# loop over lines in file
+    for i, line in enumerate(f):
+    	# split the lines into the 3 values we are interested in
+        tmp = line.split("\n")[0].split("#")[0].split("; ")
+        # line broken if we get back less than three values
+        if len(tmp) < 3:
+        	# next line
+            continue
         try:
+			# try converting strings into numbers
             pos = int(tmp[1])
             val = float(tmp[2])
-            if (pos == 1 or pos == 2) and val > 0:
+            # if these numbers are matching our format, then store them
+            if val>0 and (pos == 1 or pos == 2): 
                 loc[pos-1].append(val)
+        # if something fails, then the line was broken
         except ValueError:
             continue
-    valid[0] = valid[0]+1
+        # increase number of correct lines
+        valid[0] = valid[0]+1
 
+# output information
+print("File: " + str(filename) + " with " + str(valid[0]) + " lines")
+# for both locations calculate mean and output info
 for i in range(2):
     valid[i+1] = len(loc[i])
     mean.append(scipy.stats.mstats.gmean(np.asarray(loc[i])))
-
-print("File: " + str(filename) + " with " + str(valid[0]) + " lines")
-for i in range(2):
     print("Valid values Loc" + str(i) + ": " + str(valid[i+1]) + " with GeoMean: " + str(mean[i]))
 
