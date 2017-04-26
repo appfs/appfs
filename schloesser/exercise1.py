290b8ef64dd923eabae6223092baac5750f2e1c3
Franziska Schlösser <schloesser@zib.de>
2017-04-26 12:58:22 +0200

    now that i understood the task...

--- schloesser/exercise1.py
+++ schloesser/exercise1.py
@@ -2,8 +2,9 @@
 
 import numpy as np
 import scipy, scipy.stats
+import sys
 
-filename = "ex1-8.dat"
+filename = sys.argv[1]
 
 f = open(filename, "r")
 valid = [0,0,0]
@@ -11,23 +12,17 @@ loc = [[],[]]
 mean = []
 for i, line in enumerate(f):
     tmp = line.split("\n")[0].split("#")[0].split("; ")
-    valid[0] = valid[0]+1
     if len(tmp) < 3:
         continue
-    if tmp[1] != "":
+    if tmp[1] != "" and tmp[2] != "":
         try:
-            tmp[1] = int(tmp[1])
-            if tmp[1] == 1 or tmp[1] == 2:
-                loc[1].append(tmp[1])
+            pos = int(tmp[1])
+            val = float(tmp[2])
+            if (pos == 1 or pos == 2) and val > 0:
+                loc[pos-1].append(val)
         except ValueError:
-            pass
-    if tmp[2] != "":
-        try:
-            tmp[2] = float(tmp[2])
-            if tmp[2] > 0:
-                loc[0].append(tmp[2])
-        except ValueError:
-            pass
+            continue
+    valid[0] = valid[0]+1
 
 for i in range(2):
     valid[i+1] = len(loc[i])
