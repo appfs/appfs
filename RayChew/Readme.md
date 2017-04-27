11cae28bedbdd132f11fc7d197ab8839f5f17c9b
RayC <ray.chew@campus.tu-berlin.de>
2017-04-27 10:30:44 +0200

    updated ex1.cxx for better exception handling, updated readme.md

--- RayChew/Readme.md
+++ RayChew/Readme.md
@@ -1,21 +1,9 @@
 # Exercise 1
 Ray Chew (380421)
-1. **ex1.cxx** is a "faster" method using only getline() with poorer handling of 
-exceptions. The geometric mean is calculated favouring arithmetic multiplication 
-until value is close-to-overflow, after which log is taken.
-    * Time taken: ~28s.
-    * Valid Values at Loc1: 50004747.
-    * Valid Values at Loc2: 49994854.
-    * Lines skipped: 1631.
-2. **ex1_stringstream.cxx** is a "slower" method using istringstream() with 
-better handling of exceptions. The geometric mean is calculated with the same 
-method as in ex1.cxx.
-    * Time taken: ~1m 25s.
-    * Valid Values at Loc1: 50005278.
-    * Valid Values at Loc2: 49995372.
-    * Lines skipped: 583.
-3. **ex1_logall.cxx**, the geometric mean is calculated favouring the log() 
-function, i.e. all values are logged and summed. To test the feasibility of 
-using the log() function. Time taken: ~1m 38s.
-4. **ex1_nolog.cxx**, the geometric mean is calculated favouring arithmetic 
-methods, avoiding the use of the log() function entirely. Time taken: ~1m 28s.
+* **ex1.cxx** accepts data filename as argument. The geometric mean is 
+calculated favouring arithmetic multiplication until value is close-to-overflow, 
+after which log is taken. Does not handle cases with space(s) in SeqNo.
+    * Time taken: ~1m 45s.
+    * Valid Values at Loc1: 50004653.
+    * Valid Values at Loc2: 49994758.
+    * Lines skipped: 1824.
--- RayChew/ex1.cxx
+++ RayChew/ex1.cxx
@@ -1,34 +1,60 @@
 #include <iostream> // cout
 #include <fstream> // ifstream
+#include <sstream> // istringstream
 #include <math.h> // log;exp
+#include <vector> // vector
+#include <algorithm> // for_each
 using namespace std;
 
-int main(){
-    ifstream file("ex1.dat"); // open file.
-    string str,seqno; // define variables and consts.
-    int loc1=0, loc2=0, loc, length=100001235;
+int main(int argc, char* argv[]){
+    string str, a,b,c; // define variables and constants.
+    int i=0, loc;
     double val1=1.0, val2=1.0, val, logval1=0, logval2=0;
     const double valMax = 1e64;
-
-    for(int j = 0; j < 3; j++){getline(file,str);} // loop over first 3 lines.
+    vector<int> vectorSeqNo1,vectorSeqNo2,vectorLoc1,vectorLoc2;
+    vector<double> vectorVal1, vectorVal2;
     
-    while(getline(file,seqno,';')){ // get seq. no, discard.
-      getline(file,str,';'); // get location.
-      loc = (int)str[1]-48; // convert to int.
-      getline(file,str); // get value.
-      val = stof(str); // convert to float.
-      if ((loc == 1)&&(isnan(val)==false)){  // sort by location.
-	loc1 += 1; // update count for respective location.
-	val1 *= val; // get values multiplied until close-to-overflow.
-	if (val1 > valMax){logval1+=log(val1);val1=1.0;} // then log it and add onto logval.
+    ifstream file(argv[1]); // open file. Filename as argument of main.
+    
+    // loop over lines in file until end-of-file.
+    while (getline(file,str)){
+      if (str[0]=='#'||str.empty()){;}else{ // check if line should be ignored, if first char is '#' or empty line.
+	istringstream ss(str); // get columns.
+	ss >> a >> b >> c; // split columns at spaces. Does not handle cases with spaces in SeqNo.
+	val = stod(c); 
+	if ((any_of(b.begin(),b.end(),::isdigit)) && (!isnan(val))){ // error handling. loc must be digit, and nan values are not allowed.
+	  loc = stoi(b);
+	  if(loc==1){ // create data array corresponding to loc=1.
+	    vectorSeqNo1.push_back(stoi(a));  // storing SeqNo to have data structure of file... Not sure if necessary.
+	    vectorLoc1.push_back(loc); // store Loc for loc count. 
+	    vectorVal1.push_back(val); // store Values for GeoMean calculations.
+	  }
+	  else if (loc==2){ // create data array corresponding to loc=2. Same as in loc=1.
+	    vectorSeqNo2.push_back(stoi(a));
+	    vectorLoc2.push_back(loc);
+	    vectorVal2.push_back(val);
+	  }
+	  //else{if(!isnan(val)){cout<<"str: "<<str<<"      b:"<<b<<" c: "<<c<<endl;}} //show the exceptions that were not handled.
+	}
       }
-      else if ((loc == 2)&&(isnan(val)==false)){
-	loc2 += 1;
-	val2 *= val;
-	if (val2 > valMax){logval2+=log(val2);val2=1.0;}
-      }
+      loc=0; i++; // update counter for line count, and reset location for lines skipped.
     }
-    cout << "File: ex1.dat with " <<  length << " lines" << endl; // output results.
-    cout << "Valid values Loc1: " << loc1 << " with GeoMean: " << exp((logval1+log(val1))/loc1) << endl;
-    cout << "Valid values Loc2: " << loc2 << " with GeoMean: " << exp((logval2+log(val2))/loc2) << endl;
+    
+    // loop through vector containing values. Calculate GeoMean.
+    for_each(vectorVal1.begin(), vectorVal1.end(), [&] (double val){
+      val1 *= val; // multiply the values of the vector until close-to-overflow,
+      if (val1 > valMax){logval1+=log(val1);val1=1.0;} // then log it and add onto logval.
+    });
+    
+   // same as in for loc=1.
+    for_each(vectorVal2.begin(), vectorVal2.end(), [&] (double val){ 
+      val2 *= val;
+      if (val2 > valMax){logval2+=log(val2);val2=1.0;}
+    });
+   
+   // output results.
+    cout << "File: ex1.dat with " <<  i << " lines" << endl;
+    cout << "Valid values Loc1: " << vectorLoc1.size() << " with GeoMean: " << exp((logval1+log(val1))/vectorLoc1.size()) << endl;
+    cout << "Valid values Loc2: " << vectorLoc2.size() << " with GeoMean: " << exp((logval2+log(val2))/vectorLoc2.size()) << endl;
+    return 0;
 }
