0a895b0d2dfead16ce39d6127b7a5ca06be28719
Dinsel <dinsel@.zib.de>
2017-04-27 10:13:33 +0200

    resolved?

--- Dinsel/ex1/cGetData.pyx
+++ Dinsel/ex1/cGetData.pyx
@@ -3,34 +3,24 @@ import numpy as np
 
 def getData(fname):
     """."""
-    loc1 = 0
-    loc2 = 0
-    c1 = 0
-    c2 = 0
+    vals1 = []
+    vals2 = []
     # e = 0
-    with open(fname) as infile:
+    with open(fname, 'r') as infile:
         for line in infile:
-            if not line.startswith('#'):
-                try:
-                    # line --> array
-                    data = line.strip().split(';')
-                    # check if idx in valid
-                    idx = int(data[0])
-                    # check if value is valid
-                    num = np.log(np.float(data[2]))
-                    # check for location and if num is NaN
-                    if data[1].strip() == '1' and not np.isnan(num):
-                        loc1 += num
-                        c1 += 1
-                    elif data[1].strip() == '2' and not np.isnan(num):
-                        loc2 += num
-                        c2 += 1
-                except (IndexError, ValueError, AttributeError):
-                    # IndexError - missing ';'
-                    # ValueError - NaN
-                    # AttributeError - ['']
-                    # e += 1
-                    continue
-
+            # line --> array
+            data = line.strip().split(';')
+            try:
+                # check if idx is valid or throw exception
+                idx = int(data[0])
+                num = np.float(data[2])
+                if data[1].strip() == '1' and num > 0:
+                    vals1.append(num)
+                elif data[1].strip() == '2' and num > 0:
+                    vals2.append(num)
+            except (IndexError, ValueError, AttributeError):
+                # FIXME: this:
+                # e += 1
+                continue
     # print("{} exceptions were made due to errors.".format(e))
-    return (loc1, c1), (loc2, c2)
+    return vals1, vals2
--- Dinsel/ex1/setup.py
+++ Dinsel/ex1/setup.py
@@ -1,6 +1,26 @@
 from distutils.core import setup
 from Cython.Build import cythonize
-
+import numpy
+# import sys
+#
+# def goCompile(fname):
+#     """Insert fname into setup."""
+fname = 'cGetData.pyx'
+# fname = '/OPTI/bzfdinse/trr154-dev/Z02/tools/convert_example/convertNet.pyx'
 setup(
-    ext_modules=cythonize("cGetData.pyx"),
-)
+    name=fname.split('.')[0],
+    ext_modules=cythonize(fname),
+    )
+
+# def main(argv):
+#     """handle stuff."""
+#     import argparse
+#     parser = argparse.ArgumentParser(description="Try to compile python to cython with given .py")
+#
+#     parser.add_argument('file', help='file to cythonize', action='store')
+#
+#     goCompile(parser.parse_args())
+#
+#
+# if __name__ == '__main__':
+#     main(sys.argv[1:])
