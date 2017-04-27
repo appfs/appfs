6da7c78db58bd1fed83e1639641c2698c668fa4f
Dinsel <dinsel@.zib.de>
2017-04-27 08:08:40 +0200

    hope to resolve conflict...

--- Dinsel/ex1/ex1.py
+++ Dinsel/ex1/ex1.py
@@ -11,42 +11,32 @@ import numpy as np
 import sys
 import time
 import datetime
+from scipy.stats.mstats import gmean
 
 
 def getData(fname):
     """."""
-    loc1 = 0
-    loc2 = 0
-    c1 = 0
-    c2 = 0
+    vals1 = []
+    vals2 = []
     # e = 0
     with open(fname, 'r') as infile:
         for line in infile:
-            # e += 1
-            if not line.startswith('#'):
-                try:
-                    # line --> array
-                    data = line.strip().split(';')
-                    # check if idx in valid
-                    idx = int(data[0])
-                    # check if value is valid
-                    num = np.log(np.float(data[2]))
-                    # check for location and if num is NaN
-                    if data[1].strip() == '1' and not np.isnan(num):
-                        loc1 += num
-                        c1 += 1
-                    elif data[1].strip() == '2' and not np.isnan(num):
-                        loc2 += num
-                        c2 += 1
-                except (IndexError, ValueError, AttributeError):
-                    # IndexError - missing ';'
-                    # ValueError - NaN
-                    # AttributeError
-                    # FIXME: this:
-                    # e += 1
-                    continue
+            # line --> array
+            data = line.strip().split(';')
+            try:
+                # check if idx is valid or throw exception
+                idx = int(data[0])
+                num = np.float(data[2])
+                if data[1].strip() == '1' and num > 0:
+                    vals1.append(num)
+                elif data[1].strip() == '2' and num > 0:
+                    vals2.append(num)
+            except (IndexError, ValueError, AttributeError):
+                # FIXME: this:
+                # e += 1
+                continue
     # print("{} exceptions were made due to errors.".format(e))
-    return (loc1, c1), (loc2, c2)
+    return vals1, vals2
 
 
 def hTime(num):
@@ -59,26 +49,17 @@ def getGMean(args):
     # start timing
     # cython = False
     t0 = time.time()
-    # if args.cython:
-    #     try:
-    #         import cGetData
-    #         cython = True
-    #     except ImportError:
-    #         print("could not upload the compiled .so... but i could compile")
-    #         cython = False
-    #         # import os
-    #         # os.system('python setup.py build_ext --inplace')
-    #         # import cGetData
-    #         # cython = True
-    #
-    # # if cython:
-    #     vals1, vals2 = cGetData.getData(args.file)
-    # else:
-    vals1, vals2 = getData(args.file)
+    if args.cython:
+        import cGetData
+        vals1, vals2 = cGetData.getData(args.file)
+    else:
+        vals1, vals2 = getData(args.file)
     print("got arrays in {}".format(hTime(time.time() - t0)))
 
-    print("{} valid values of loc1 with GeoMean: {}".format(vals1[1], np.exp(vals1[0]/vals1[1])))
-    print("{} valid values of loc2 with GeoMean: {}".format(vals2[1], np.exp(vals2[0]/vals2[1])))
+    print("{} valid values of loc1 with GeoMean: {}".format(len(vals1), gmean((vals1))))
+    print("{} valid values of loc2 with GeoMean: {}".format(len(vals2), gmean((vals2))))
+    # print("{} valid values of loc1 with GeoMean: {}".format(vals1[1], np.exp(vals1[0]/vals1[1])))
+    # print("{} valid values of loc2 with GeoMean: {}".format(vals2[1], np.exp(vals2[0]/vals2[1])))
 
     t1 = time.time()
     print("{} took {} to solve.".format(">"*10, hTime(t1-t0)))
@@ -102,7 +83,7 @@ def main(argv):
 
     parser.add_argument('file', help='file to read', action='store')
 
-    # parser.add_argument('-c', '--cython', help='use the cython lib (not as fast as expected)', action='store_true', dest='cython')
+    parser.add_argument('-c', '--cython', help='use the cython lib (not as fast as expected)', action='store_true', dest='cython')
 
     getGMean(parser.parse_args())
 
