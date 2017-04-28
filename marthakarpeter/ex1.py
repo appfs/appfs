5df2fd99e16168b400a9778014ca73a94e82ac85
marthakarpeter <m.karpeter@campus.tu-berlin.de>
2017-04-28 20:05:38 +0200

    pull

--- marthakarpeter/ex1.py
+++ marthakarpeter/ex1.py
@@ -1,29 +1,40 @@
-#ex1
+#ex1 APPFS Martha Karpeter 367847
+
 from math import sqrt
 import decimal
-Dec = decimal.Decimal
+Decim = decimal.Decimal
 
 file = open("ex2.dat","r") 
-lines = file.readlines() 
+Data = file.readlines() #reads lines from the data set
 decimal.getcontext().prec = 10
 
-def GM(Data):
-    n = Dec(len(Data))
-    gm = 1
-    for d in Data:
-        gm*=d**(Dec(1)/n)
-    return(gm)
-
-lenloc1,lenloc2 = 0,0
-GM1, GM2 = Dec(1), Dec(1)
+#devide Data into Location1 and Location2
+Location1 = [Decim(line[line.index(";")+5:]) for line in Data if ";" in line and line[line.index(";")+5:].count(".")==1 and line.count(" ") == 2 and int(line[line.index(";")+2]) == 1]
+Location2 = [Decim(line[line.index(";")+5:]) for line in Data if ";" in line and line[line.index(";")+5:].count(".")==1 and line.count(" ") == 2 and int(line[line.index(";")+2]) == 2]
 
 
+#computes Geometric Mean of a given dataset, returns type decimal.Decimal
+def GeometricMean(dataset):
+    length = Decim(len(dataset))
+    result = Decim(1)
+    for line in dataset:
+        result *= line**(Decim(1)/length)
+    return(Decim(result))
 
-Location1 = [Dec(line[line.index(";")+5:]) for line in lines[3:-1] if int(line[line.index(";")+2]) == 1]
-Location2 = [Dec(line[line.index(";")+5:]) for line in lines[3:-1] if int(line[line.index(";")+2]) == 2]
+#get the geometric mean for each location
+GeometricMean1 = GeometricMean(Location1)
+GeometricMean2 = GeometricMean(Location2)
 
-GM1 = GM(Location1)
-GM2 = GM(Location2)
+#output 
+print("File ex2.dat with", len(Data), "lines")
+print('Valid values Loc1:', len(Location1), 'with GeoMean:', "%.4f"%GeometricMean1)
+print('Valid values Loc2:', len(Location2), 'with GeoMean:', "%.4f"%GeometricMean2)
 
-print(GM1, GM2)
 file.close()
+
+
+# File ex1.dat with 10000128 lines
+# Valid values Loc1: 4998044 with GeoMean: 36.7802
+# Valid values Loc2: 5000938 with GeoMean: 36.7666
+
+# real    9m42.231s
