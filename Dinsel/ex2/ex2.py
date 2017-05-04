00bb0471a0d80c126e65dc306e9b8f5e1924cede
Dinsel <dinsel@.zib.de>
2017-05-04 10:54:58 +0200

    submission ex2

--- Dinsel/ex2/ex2.py
+++ Dinsel/ex2/ex2.py
@@ -22,35 +22,37 @@ class ParseXML():
     def __init__(self, xml, xsd, verbose):
         self.fileXML = xml
         self.fileXSD = xsd
+        self.verbose = verbose
         self.dict = defaultdict(list)
 
-        try:  # to open the xml file and proceed if validation is true
-            self.readXSD()
-            self.readXML()
+        try:  # to open the xml file with schema
+            schema = self.readXSD()
+            self.readXML(schema)
             self.readAttributes()
             self.writeCSV()
         except (XMLSchemaParseError, XMLSyntaxError) as e:
             # stream needs file object
             sys.stderr = open('err.log', 'w')
             sys.stderr.write(str(e))
-            if verbose:
-                print("ERROR: {}".format(e))
-
-        if verbose:
-            for k, v in self.dict.items():
-                # stdout catches its file object from command line pipe
-                print(k, v, file=sys.stdout)
+            # possible to get the data although not valid
+            yesNo = input("Encountered error (see err.log). Continue anyway? [yn] ")
+            if yesNo == 'y':
+                self.readXML()
+                self.readAttributes()
+                self.writeCSV()
+            elif yesNo == 'n':
+                sys.exit()
 
     def readXSD(self):
         """Open a scheme file to validate the to-be-read file."""
         schema = etree.parse(self.fileXSD)
-        self.schema = etree.XMLSchema(schema)
+        return etree.XMLSchema(schema)
         # only returns True or False
         # print(self.schema.validate(etree.parse(self.fileXML)))
 
-    def readXML(self):
+    def readXML(self, schema=None):
         """Open XML file and return the root."""
-        parser = etree.XMLParser(remove_comments=True, schema=self.schema)
+        parser = etree.XMLParser(remove_comments=True, schema=schema)
         # validation happens with the schema argument during parsing or throw
         # an error to see whats wrong (in init)
         tree = objectify.parse(self.fileXML, parser=parser)
@@ -62,7 +64,7 @@ class ParseXML():
         # gasDay - depth 0
         nodes = self.rootXML.find("{" + nsmap[None] + "}gasDay")
         self.date = nodes.get('date')
-        preHourValue = int(nodes.get('gasDayStartHourInUTC'))
+        startingHour = int(nodes.get('gasDayStartHourInUTC'))
         # boundaryNode - depth 1
         for node in nodes:
             # time - depth 2
@@ -70,16 +72,21 @@ class ParseXML():
                 # amountOfPower - depth 3
                 aop = elem.find("{" + nsmap[None] + "}amountOfPower")
                 # set id as dictionary key and tuple the hour + value
-                self.dict[node.get('id')].append((int(elem.get('hour')) + preHourValue, float(aop.get('value'))))
+                self.dict[node.get('id')].append((int(elem.get('hour')) + startingHour, float(aop.get('value'))))
 
     def writeCSV(self):
         """Print data to csv."""
-        with open('.'.join(self.fileXML.split('/')[1].split('.')[:-1]) + '.csv', 'w', newline='') as out:
+        fName = '.'.join(self.fileXML.split('/')[1].split('.')[:-1]) + '.csv'
+        with open(fName, 'w', newline='') as out:
             writer = csv.writer(out, delimiter=';')
+            # sort dictionary by key
             for key, vals in sorted(self.dict.items()):
                 writer.writerow(['# ' + key])
                 for val in vals:
                     writer.writerow([self.date, val[0], val[1]])
+
+        if self.verbose:
+            print("Saved as {}".format(fName), file=sys.stdout)
 
 
 def main(argv):
