7b66a915a6b3627ebfaab0ec08ffcaa50ce4422c
Alessandro Bartsch <alebartsch@hotmail.com>
2017-05-04 15:42:25 +0200

    makefile for ex1

---
+++ Sanny/ex1/appfs/src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/LocationValues.cpp \
+../src/ex1.cpp 
+
+OBJS += \
+./src/LocationValues.o \
+./src/ex1.o 
+
+CPP_DEPS += \
+./src/LocationValues.d \
+./src/ex1.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -std=c++1y -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
