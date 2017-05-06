79097bdc8a3c91001d5f6655899cb762d6bafa39
RayC <ray.chew@campus.tu-berlin.de>
2017-05-06 18:12:02 +0200

    updated ex1.cxx for same numbers, ex2.cxx for validation.

--- RayChew/Ex1/ex1.cxx
+++ RayChew/Ex1/ex1.cxx
@@ -7,37 +7,54 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
-    string str, a,b,c; // define variables and constants.
-    int i=0, loc;
+    string str, a,b,c, token; // define variables and constants.
+    int i=0,d=1, loc;
     double val1=1.0, val2=1.0, val, logval1=0, logval2=0;
     const double valMax = 1e64;
-    vector<int> vectorSeqNo1,vectorSeqNo2,vectorLoc1,vectorLoc2;
+    vector<int> vectorLoc1,vectorLoc2;
     vector<double> vectorVal1, vectorVal2;
     
     ifstream file(argv[1]); // open file. Filename as argument of main.
     
     // loop over lines in file until end-of-file.
+    //while (i < 1000){
+    //  getline(file,str);
     while (getline(file,str)){
-      if (str[0]=='#'||str.empty()){;}else{ // check if line should be ignored, if first char is '#' or empty line.
+      //str = str.substr(0, str.find("#", 0));
+      if ((str[0]=='#')||(str.empty())){;}else{ // check if line should be ignored, if first char is '#' or empty line.
 	istringstream ss(str); // get columns.
-	ss >> a >> b >> c; // split columns at spaces. Does not handle cases with spaces in SeqNo.
+	//ss >> a >> b >> c; // split columns at spaces. Does not handle cases with spaces in SeqNo.
+	int j=0;
+	while(getline(ss,token,';')){
+	  if (j==0){a = token;}
+	  else if (j==1){b = token;}
+	  else if (j==2){c = token;}
+	  else {d = 0;}
+	  j++;
+	}
+	int k = 0;
+	while (a[k]){
+	  if (isspace(a[k])){
+	    a=
 	val = stod(c); 
-	if ((any_of(b.begin(),b.end(),::isdigit)) && (!isnan(val))){ // error handling. loc must be digit, and nan values are not allowed.
+	if (d != 0){
+	//if ((any_of(b.begin(),b.end(),::isdigit)) && (!isnan(val))){ // error handling. loc must be digit, and nan values are not allowed.
+	if ((any_of(b.begin(),b.end(),::isdigit)) && (!isnan(val))){
 	  loc = stoi(b);
 	  if(loc==1){ // create data array corresponding to loc=1.
-	    vectorSeqNo1.push_back(stoi(a));  // storing SeqNo to have data structure of file... Not sure if necessary.
 	    vectorLoc1.push_back(loc); // store Loc for loc count. 
 	    vectorVal1.push_back(val); // store Values for GeoMean calculations.
 	  }
 	  else if (loc==2){ // create data array corresponding to loc=2. Same as in loc=1.
-	    vectorSeqNo2.push_back(stoi(a));
 	    vectorLoc2.push_back(loc);
 	    vectorVal2.push_back(val);
 	  }
 	  //else{if(!isnan(val)){cout<<"str: "<<str<<"      b:"<<b<<" c: "<<c<<endl;}} //show the exceptions that were not handled.
 	}
+	}
       }
       loc=0; i++; // update counter for line count, and reset location for lines skipped.
+      d=1;
     }
     
     // loop through vector containing values. Calculate GeoMean.
