bf79df9c9c1d98614778139ff8016458b7ac5097
Thekm93 <maxmrichter@gmail.com>
2017-05-08 22:30:04 +0200

    ex1 updated

--- richter/ex1.java
+++ richter/ex1.java
@@ -1,11 +1,12 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 
 public class ex1 {
 
+	static float[] pos1 = new float[51000000];
+	static float[] pos2 = new float[51000000];
+	
 	public static void main(String[] args) {
 			
 		long startTime = System.currentTimeMillis();
@@ -20,62 +21,96 @@ public class ex1 {
 		}
 			
         String line = "";
-        String location;
-        double value;
+        int seqnum;
+        int location;
+        Float value;
         double sumOfLog1 = 0;
         double sumOfLog2 = 0;
         double geoAveragePos1, geoAveragePos2;
         int validDataPointsPosition1 = 0;
         int validDataPointsPosition2 = 0;
-        long progress = 0;
+        long progress = -1;
 	        
 		        
         while (line != null) {
         	
 	    	progress++;
-	    	
+
 	    	try{
-	    		line = br.readLine().replaceAll("\\s+", "");
+	    		
+	    		line = br.readLine();
+	    		
 	    	}
 	    	catch(Exception e){
+	    		
 	    		break;
-	    	}
-	
-	    	// ignore line if comment
-	    	if(isComment(line))
-	    		continue;
-	
-	        String parts[] = line.split(";");
-	    	
-	        // ignore line if the given format is not used in the line
-	        if(parts.length != 3)
-	        	continue;
-	        
-	    	location = parts[1];
-	    	
-	    	// ignore line if location is not 1 or 2
-	    	if(!(location.equals("1") || location.equals("2")))
-	    		continue;
-	        
-	    	value = Double.parseDouble(parts[2]);
-	    	
-	    	// ignore line of value is less than zero or NaN
-	    	if(value < 0 || Double.isNaN(value))
-	    		continue;
-	    	
-	    	// since we care for the *geometrical* average (product), we can work with logarithms
-	    	if(location.equals("1")){
-	    		sumOfLog1 += Math.log(value);
-	    		validDataPointsPosition1++;
+	    		
 	    	}
 	    	
-	    	else if(location.equals("2")){
-	    		sumOfLog2 += Math.log(value);
-	    		validDataPointsPosition2++;
+	    	if(line == null)
+	    		continue;
+	
+	    	
+	    	try{
+	    		
+		    	// removes characters after a #
+	    		line = line.split("#")[0];
+	    		
+	    		String parts[] = line.split(";");
+	    		seqnum = Integer.valueOf(parts[0].trim());
+		    	location = Integer.valueOf(parts[1].trim());
+		    	value = Float.valueOf(parts[2].trim());
+		    	
+		    	// ignore line if location is not 1 or 2
+		    	if(!( location == 1 || location == 2)){
+//		    		System.err.println("Invalid location format.");
+		    		continue;
+		    	}
+
+		    	
+		    	// ignore line of value is less than zero or NaN
+		    	if(value < 0 || Double.isNaN(value)){
+//		    		System.err.println("Invalid number format.");
+		    		continue;
+		    	}
+		    	
+		    	// since we care for the *geometrical* average (product), we can work with logarithms
+		    	if(location == 1){
+//		    		sumOfLog1 += Math.log(value);
+//		    		pos1.add(value);
+		    		pos1[validDataPointsPosition1] = value;
+		    		validDataPointsPosition1++;
+		    	}
+		    	
+		    	else if(location == 2){
+//		    		sumOfLog2 += Math.log(value);
+//		    		pos2.add(value);
+		    		pos2[validDataPointsPosition2] = value;
+		    		validDataPointsPosition2++;
+		    	}
+	    		
+	    	}
+	    	
+	    	catch(Exception e){
+
+	    		continue;
+	    		
 	    	}
     	
         }
 		        
+        for(int i = 0; i < validDataPointsPosition1; i++){
+        	
+        	sumOfLog1 += Math.log(pos1[i]);
+        	
+        }
+        
+        for(int i = 0; i < validDataPointsPosition2; i++){
+        	
+        	sumOfLog2 += Math.log(pos2[i]);
+        	
+        }
+        
 		geoAveragePos1 = Math.exp(sumOfLog1 / validDataPointsPosition1);
 		geoAveragePos2 = Math.exp(sumOfLog2 / validDataPointsPosition2);
 		        
@@ -86,16 +121,6 @@ public class ex1 {
 	    long elapsedTime = System.currentTimeMillis() - startTime;
 		
 	    System.out.println("Needed time: " + elapsedTime / 1000 + "sek"); 
-}
-
-	private static boolean isComment(String line){
-		
-		if(line.startsWith("#") || line == null){
-			return true;
-		}
-		
-		return false;
-		
 	}
 	
 }
