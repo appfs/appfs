5c877b9f8ddb8cfa2af57d6c40a9372195d3b833
AlexanderRettkowski <alexander.rettkowski@capgemini.com>
2017-05-08 14:56:19 +0200

    Update ex1.cpp
    Switched to qi (part of boost) tokenizer and corrected the filtering.

--- rettkowski/ex1.cpp
+++ rettkowski/ex1.cpp
@@ -1,71 +1,115 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <boost/algorithm/string.hpp>
-#include <boost/lexical_cast.hpp>
+#include <boost/config/warning_disable.hpp>
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix_core.hpp>
+#include <boost/spirit/include/phoenix_operator.hpp>
+#include <boost/spirit/include/phoenix_object.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/fusion/include/io.hpp>
 
-using namespace std;
-using namespace boost;
+#include <iostream>
+#include <string>
+#include <complex>
+#include <fstream>
+
+
+namespace ex1
+{
+	namespace qi = boost::spirit::qi;
+	namespace ascii = boost::spirit::ascii;
+	struct line
+	{
+		int id;
+		int location;
+		double value;
+	};
+}
+
+
+BOOST_FUSION_ADAPT_STRUCT(
+	ex1::line,
+	(int, id)
+	(int, location)
+	(double, value)
+)
+
+namespace ex1
+{
+	template <typename Iterator>
+	struct line_parser : qi::grammar<Iterator, line(), ascii::space_type>
+	{
+		line_parser() : line_parser::base_type(start)
+		{
+			using qi::int_;
+			using qi::double_;
+
+			start %= int_ >> ';' >> int_ >> ";" >> double_;
+		}
+
+		qi::rule<Iterator, line(), ascii::space_type> start;
+	};
+}
 
 int main(int argc, char *argv[])
 {
-    long loc1size = 0, loc2size = 0, numberOfLines = 0;
+	using boost::spirit::ascii::space;
+	typedef std::string::const_iterator iterator_type;
+	typedef ex1::line_parser<iterator_type> line_parser;
 
-    ifstream file(argv[1]);
-    string str;
+	line_parser g;
+	std::string str;
 
-    double loc1log = 0.0, loc2log = 0.0, value = 0;
-    int pos, location = 0;
-    stringstream tmp;
+	std::ifstream file(argv[1]);
+	int success_lines = 0;
 
-    while (getline(file, str))
-    {
-        numberOfLines++;
-        if (!starts_with(str, "#") && str.length() >= 1)
-        {
-            pos = str.find_first_of(';');
-            if (pos != string::npos)
-            {
-                str = str.substr(pos + 2);
-                pos = str.find_first_of(';');
-                if (pos != string::npos)
-                {
-                    tmp.str(str.substr(0, pos));
-                    if (strchr(tmp.str().c_str(), ' '))
-                    {
-                        continue;
-                    }
-                    tmp >> location;
-                    tmp.clear();
-                    tmp.str(str.substr(pos + 2));
-                    if (strchr(tmp.str().c_str(), ' '))
-                    {
-                        continue;
-                    }
-                    tmp >> value;
-                    tmp.clear();
-                }
-            }
-            if (isnormal(value))
-            {
-                if (location == 1)
-                {
-                    loc1log += log2(value);
-                    loc1size++;
-                }
-                else if (location == 2)
-                {
-                    loc2log += log2(value);
-                    loc2size++;
-                }
-            }
-        }
-    }
+	std::vector<double> values[2];
+	double mean[2]{ 0.0, 0.0 };
 
-    double loc1mean = exp2(loc1log / loc1size);
-    double loc2mean = exp2(loc2log / loc2size);
+	long numberOfLines = 0;
 
-    printf("File: %s with %ld lines\n", argv[1], numberOfLines);
-    printf("Valid values Loc1: %ld with GeoMean: %f\n", loc1size, loc1mean);
-    printf("Valid values Loc2: %ld with GeoMean: %f\n", loc2size, loc2mean);
+	while (getline(file, str))
+	{
+		numberOfLines++;
+
+		ex1::line emp;
+		std::string::const_iterator iter = str.begin();
+		std::string::const_iterator end = str.end();
+		bool r = phrase_parse(iter, end, g, space, emp);
+
+		if (r && iter == end)
+		{
+			if (std::isnan(emp.value) || emp.value <= 0 || emp.location < 1 || emp.location > 2)
+				continue;
+
+			values[--emp.location].push_back(emp.value);
+			/*std::cout << "-------------------------\n";
+			std::cout << "Parsing succeeded\n";
+			std::cout << "got: " << boost::fusion::as_vector(emp) << std::endl;
+			std::cout << "\n-------------------------\n";*/
+			success_lines++;
+		}
+		else
+		{
+			/*std::cout << "-------------------------\n";
+			std::cout << "Parsing failed\n";
+			std::cout << str << std::endl;
+			std::cout << "-------------------------\n";*/
+			//std::cout << str << std::endl;
+		}
+	}
+
+	file.close();
+
+	for (int i = 0; i < 2; i++)
+	{
+		for (double d : values[i])
+		{
+			mean[i] += std::log2(d);
+		}
+		mean[i] = std::exp2(mean[i] / values[i].size());
+	}
+
+	printf("File: %s with %ld lines\n", argv[1], numberOfLines);
+	printf("Valid values Loc1: %ld with GeoMean: %f\n", values[0].size(), mean[0]);
+	printf("Valid values Loc2: %ld with GeoMean: %f\n", values[1].size(), mean[1]);
+	return 0;
 }
