f34aa9e0bd1d9448ea5a215cae4b7ca24cccfd0a
Alessandro Bartsch <alebartsch@hotmail.com>
2017-05-15 22:07:16 +0200

    another try

--- Sanny/ex3/src/quine.cpp
+++ Sanny/ex3/src/quine.cpp
@@ -4,24 +4,29 @@
 using namespace std;
 
 int main(){
-string s="#include <iostream>|"
-"#include <string>|"
-"|"
-"using namespace std;|"
-"int main(){|"
-"string s=;|"
-"|"
-  "for(unsigned int i=0;i<s.length();i++){|"
-     "if(s[i]=='|'){|"
-       "cout<<endl;|"
-     "} else {|"
-       "cout<<s[i];|"
-     "}|"
-   "return 1;|"
-"}|";
+string s="#include <iostream>\n"
+"#include <string>\n"
+"\n"
+"using namespace std;\n"
+"int main(){\n"
+"string s=;\n"
+"\n"
+  "for(unsigned int i=0;i<s.length();i++){\n"
+     "int count = 0;\n"
+     "if(s[i]=='\n'){\n"
+       "cout<<endl;\n"
+     "} else {\n"
+       "cout<<s[i];\n"
+     "}\n"
+     "count++;\n"
+   "return 1;\n"
+"}\n";
 
    for(unsigned int i=0;i<s.length();i++){
-	   if(s[i]=='|'){
+	   if(126 == i){
+		   cout << s;
+	   }
+	   if(s[i]=='\n'){
 		   cout<<endl;
 	   } else {
 		   cout<<s[i];
