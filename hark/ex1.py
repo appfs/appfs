3994f6cc2c281fdc39488e31073cf732ba917e69
Johann Nikolai Hark <johann.nikolai.hark@fokus.fraunhofer.de>
2017-05-17 17:23:55 +0200

    adapted ex1 to correctly decline (all) invalid lines. shell executable added.

--- hark/ex1.py
+++ hark/ex1.py
@@ -1,44 +1,51 @@
 import sys
-import csv
 import math
 
-def is_valid_value(str_value):
+
+def is_valid_line(line_as_array, line_number, stripped):
     try:
-        if math.isnan(float(str_value)) or float(str_value) <= 0.0:
+        int(line_as_array[0])
+        location = int(line_as_array[1])
+        location_value = float(line_as_array[2])
+        if location not in [1, 2]:
+            print("Line {} : Illegal location: {}".format(line_number + 1, location))
             return False
-    except:
+        if math.isnan(location_value) or location_value <= 0.0:
+            print("Line {} : Invalid value".format(line_number + 1))
+            return False
+    except ValueError:
+        print("Line {} : Syntax error [{}]".format(line_number + 1, stripped))
         return False
-    return True
+    return location, location_value
+
 
 def compute_geometric_mean_from_csv(filename):
-    with open(filename, 'r') as csv_file:
-        line_reader = csv.reader(csv_file, delimiter=";", skipinitialspace=True)
-        line_count = 0
-        valid_count1 = 0
-        valid_count2 = 0
-        mean1 = 0.0
-        mean2 = 0.0
-        for line in line_reader:
-            line_count += 1
-            if line:
-                if len(line) == 3 and line[0][0] != '#':
-                    if line[1] == '1' and is_valid_value(line[2]):
-                            valid_count1 += 1
-                            mean1 += math.log(float(line[2]))
-                    elif line[1] == '2' and is_valid_value(line[2]):
-                            valid_count2 += 1
-                            mean2 += math.log(float(line[2]))
-        mean1 = math.exp(mean1/valid_count1)
-        mean2 = math.exp(mean2/valid_count2)
-    return line_count, valid_count1, valid_count2, mean1, mean2
+    valid_counts = [0, 0]
+    means = [0.0, 0.0]
+    with open(filename, "r") as file:
+        for line_count, line in enumerate(file):
+            line = line.split("#")[0]
+
+            stripped_line = line.strip()
+            if stripped_line == "":
+                continue
+
+            split_line = stripped_line.split(";")
+
+            if len(split_line) != 3:
+                print("Line {} : Syntax error [{}]".format(line_count + 1, split_line))
+                continue
+
+            if is_valid_line(split_line, line_count, stripped_line) is not False:
+                    location, value = is_valid_line(split_line, line_count, stripped_line)
+                    valid_counts[(location-1)] += 1
+                    means[(location-1)] += math.log(value)
+        means[0] = math.exp(means[0]/valid_counts[0])
+        means[1] = math.exp(means[1]/valid_counts[1])
+    return line_count, valid_counts[0], valid_counts[1], means[0], means[1]
 
 lineCount, count1, count2, geo_mean1, geo_mean2 = compute_geometric_mean_from_csv(sys.argv[1])
 
 print('File:', sys.argv[1], 'with', lineCount, 'lines')
 print('Valid values Loc1:', count1, 'with GeoMean:', geo_mean1)
 print('Valid values Loc2:', count2, 'with GeoMean:', geo_mean2)
-
-# output
-# File: ex1.dat with 100001235 lines
-# Valid values Loc1: 50004616 with GeoMean: 36.7818
-# Valid values Loc2: 49994716 with GeoMean: 36.7826
