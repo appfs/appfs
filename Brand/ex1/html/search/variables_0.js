34227e9d38af825c215c4db0c35e5aa10db3cd06
micbra89 <michael.brand89@gmx.de>
2017-05-22 13:50:46 +0200

    Add files via upload

---
+++ Brand/ex1/html/search/variables_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['category',['category',['../ex1_8cpp.html#a60863e080514b398bce285b4d087dcf1',1,'ex1.cpp']]]
+];
---
+++ Brand/ex1/html/search/variables_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['errors',['errors',['../ex1_8cpp.html#a433c7fadd13f3a906e01c9d9f5c8fb09',1,'ex1.cpp']]]
+];
