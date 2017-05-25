6c484dbebaceaa802309fab7edbe45cbf678edae
Roxana Tapia <ext-roxana.tapia@here.com>
2017-05-25 14:49:55 +0200

    Exercise 4 complete

---
+++ tapia/exercise4/output/html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "aceirwx",
+  1: "e",
+  2: "acirwx"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "namespaces",
+  2: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Namespaces",
+  2: "Functions"
+};
+
