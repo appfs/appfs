baaf6e8ac4c0aa78a29e61bc5d8921cbf47e9ca2
Roxana Tapia <ext-roxana.tapia@here.com>
2017-05-25 14:49:55 +0200

    Exercise 4 complete

--- tapia/exercise2/ex2.py
+++ tapia/exercise2/ex2.py
@@ -1,11 +1,12 @@
+"""@package docstring
+Author: Roxana Tapia Campos
+Module: Advanced practical programming skills for scientists
+SS17 - TU Berlin 
+Exercise 2: XML to CSV measured
+"""
+
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-##################################################################
-# Author: Roxana Tapia Campos                                    #
-# Module: Advanced practical programming skills for scientists   #
-# SS17 - TU Berlin                                               #
-##################################################################
 
 import sys
 
@@ -14,8 +15,10 @@ from bs4 import BeautifulSoup
 
 def xml_to_csv(data):
     """
-    Searches all suitable values for the response format:
+    Searches all suitable values (tags) to build a response with format:
     YYYY-MM-DD; HH; amountOfPower-Value
+    
+    :param data: Parsed data from XML file as a tree format.
     """
     output = list()
     gas_days = data.find_all('gasDay')
@@ -36,17 +39,21 @@ def xml_to_csv(data):
 
     write_csv(output)
 
+
 def write_csv(content_list):
+    """
+    Writes a CSV 'output.csv' file with the formatted result.
+    
+    :param content_list: A list with triplets.
+    """
     with open('output.csv','w') as file:
         for line in content_list:
             file.write(line)
             file.write('\n')
 
 if __name__ == '__main__':
-    filename=sys.argv[-1]
+    filename = sys.argv[-1]
     xml_text = open(filename).read()
     parsed_data = BeautifulSoup(xml_text, 'xml')
 
     xml_to_csv(parsed_data)
-
-    
