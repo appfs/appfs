feabc416a815d193045589f9b89a30fd581935aa
Franziska Schlösser <schloesser@zib.de>
2017-05-30 10:54:31 +0200

    integrate pylint

--- schloesser/exercise01/ex1.py
+++ schloesser/exercise01/ex1.py
@@ -1,37 +1,66 @@
 #!/usr/bin/env python
 
-## @package exercise 1 + 4 
-#  This package provides means to solving exercises 1 and 4 from the lecture appfs
+""" 
+Exercise 1 + 4
 
-import numpy as np
-import scipy, scipy.stats
+This package provides means to solving exercises 1 and 4 from the lecture appfs, summer 2017, TUB
+
+The size of the inputfile is around 2GB.
+Each line of the inputfile should consist of
+
+   - a sequence-number,
+   - a location (1 or 2),and
+   - a floating point value > 0.
+
+Empty lines are allowed. Comment lines start with a ”#”.
+Everything after a ”’#” on a line should be ignored.
+
+This module reads in and internally stores the data from the inputfile.
+
+Computes the geometric mean for each location and gives output in a form like this:
+
+    "File: ex1.dat with 100001235 lines
+    Valid values Loc1: 50004598 with GeoMean: 36.7817
+    Valid values Loc2: 49994703 with GeoMean: 36.7825"
+"""
+
 import sys
+import numpy as np
+from scipy import stats
 
-## Exercise 1 class
-#  This class provides methods for completing the first exercise of the lecture appfs, summer 2017, TUB
 class Ex1:
-    
-    ## Constructor
-    #  
-    #  @param string the name of the file containing the data. Each line has format:
-    #      seq; loc; val
-    #  where seq is a sequence number, loc is either 1 or 2 and val is a value > 0.
-    #  Empty lines or a line starting with '#' is treated as comment and ignored.
+    """ 
+    Exercise 1 class
+
+    This class provides methods for completing the first exercise of the lecture appfs,
+
+    Attributes:
+        filename: store the filename
+        total: store [number of lines, number of positions in location1, number of positions in location2]
+        loc: store values in loc1, loc2 respectively
+        n: store number of values in locations
+        mean: store the geometric mean of both locations
+    """
+
     def __init__(self, string):
-        ## store the filename
-        self.filename = string;
-        ## store [number of lines, number of positions in location1, number of positions in location2]
-        self.total = 0
-        ## store values in loc1, loc2 respectively
-        self.loc = [[],[]]
-        ## store number of values in locations
-        self.n = []
-        ## store the geometric mean of both locations
-        self.mean = []
+        """ 
+        Constructor
         
-    ## Parse a file
-    #  Write to total and loc the number of lines resp. the values in both locations
+        Args: 
+            string: the name of the file containing the data
+        """
+        self.filename = string
+        self.total = 0
+        self.loc = [[], []]
+        self.n = []
+        self.mean = []
+
     def parse_file(self):
+        """ 
+        Parse a file
+        
+        Write to total and loc the number of lines resp. the values in both locations
+        """
         # open file
         with open(self.filename, "r") as f:
             last = 0
@@ -45,53 +74,62 @@ class Ex1:
                 if len(tmp) < 3:
                     # next line
                     if not line[0] == '#' and not line == "\n":
-                        print("ERROR: Line {} does not have required format or is comment:\n{}".format(self.total, line))
+                        msg = "ERROR: Line {} does not have required format or is comment:\n{}"
+                        print(msg.format(self.total, line))
                     continue
                 try:
                         # try converting strings into numbers
                     seq = int(tmp[0])
-                    if seq != last+1: 
-                        print("WARNING: Something wrong with sequence numbers in line {}. Expected {} but found {}:\n{}".format(self.total, last+1, seq, line))
+                    if seq != last+1:
+                        msg = "WARNING: Error in sequence numbers in line {}. Expected {} but found {}:\n{}"
+                        print(msg.format(self.total, last+1, seq, line))
                     last = seq
 
                     pos = int(tmp[1])
                     val = float(tmp[2])
                     # if these numbers are matching our format, then store them
-                    if val>0 and (pos == 1 or pos == 2): 
+                    if val > 0 and (pos == 1 or pos == 2):
                         self.loc[pos-1].append(val)
                         continue
-                    else: 
-                        print("ERROR: Values of one or more numbers in line {} do not satisfy requirements:\n{}".format(self.total, line))
+                    else:
+                        msg = "ERROR: One or more values from line {} do not satisfy requirements:\n{}"
+                        print(msg.format(self.total, line))
                 # if something fails, then the line was broken
                 except ValueError:
-                    print("ERROR: Not all numbers in line {} satisfy requirements:\n{}".format(self.total, line))
+                    msg = "ERROR: One or more numbers in line {} satisfy requirements:\n{}"
+                    print(msg.format(self.total, line))
                     continue
-        
-    ## Print information
-    #  print collected information to stdout such as filename, number of lines and values and the geometric means.
+
     def print_info(self):
+        """ 
+        Print information
+        
+        print collected information to stdout such as filename, number of lines and values
+        and the geometric means.
+        """
         # output information
         print("File: " + str(self.filename) + " with " + str(self.total) + " lines")
         # for both locations calculate mean and output info
         for i in range(2):
-            print("Valid values Loc" + str(i+1) + ": " + str(self.n[i]) + " with GeoMean: " + str(self.mean[i]))
-    
-    ## Calculate geometric mean
-    #  calculate the geometric means of the values in the lists in loc
+            print("Valid values Loc {}: {} with GeoMean: {}".format(i+1, self.n[i], self.mean[i]))
+
     def calculate_means(self):
+        """ 
+        Calculate geometric mean
+        
+        calculate the geometric means of the values in the lists in loc
+        """
         for i in range(2):
             self.n.append(len(self.loc[i]))
-            self.mean.append(scipy.stats.mstats.gmean(np.asarray(self.loc[i])))
-    
-## Mainmethod
-#  run when class is executed, but not after import
-if __name__=="__main__":
+            self.mean.append(stats.mstats.gmean(np.asarray(self.loc[i])))
+
+if __name__ == "__main__":
     # read filename from commandlineargs
     filename = sys.argv[1]
-    e = Ex1(filename)
+    ex = Ex1(filename)
 
     # parse file, calculate means and output result to user
-    e.parse_file()
-    e.calculate_means()
-    e.print_info()
-
+    ex.parse_file()
+    ex.calculate_means()
+    ex.print_info()
+    print("")
