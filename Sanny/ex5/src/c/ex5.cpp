7dc7e88ade5a91b12b1823ad441eee6dc44b9860
Alessandro Bartsch <alebartsch@hotmail.com>
2017-06-03 13:43:45 +0200

    Setting up for ex5

---
+++ Sanny/ex5/src/c/ex5.cpp
@@ -0,0 +1,12 @@
+/**
+ * Exercise 5 : Read in a gph-file, computes the longest (with respect to the edge weights) shortest path from any vertex to the vertex with index 1.
+ *
+ * @author FirstSanny
+ */
+
+
+/** Reading in a Graphfile, computes the longest shortest path */
+int main(int argn, char *argv[]) {
+
+	return 0;
+}
