58d35aeb67e2aaf5c2612902d42724f52998332a
Franziska Schloesser <schloesser@zib.de>
2017-06-06 19:25:53 +0200

    using graph-tools

--- schloesser/exercise05/runall.sh
+++ schloesser/exercise05/runall.sh
@@ -9,5 +9,8 @@ for f in $FILES
 do
     echo "Processing file $f..."
     echo $f >> $OUTFILE 
+    echo "standalone"
+    (time python3 ex5_standalone.py $f) &>> $OUTFILE
+    echo "graph_tool"
     (time python3 ex5.py $f) &>> $OUTFILE
 done
