a4a5218c33de9259c71ddaeb86be32a9a70073a4
yomu92 <jo91992@web.de>
2017-06-08 17:50:58 +0200

    Update vertex.h

--- Jung/ex5/vertex.h
+++ Jung/ex5/vertex.h
@@ -1,13 +1,17 @@
 #include <vector>
 #include <utility>
 
+/**
+* @class vertex
+* @brief a vertex for the dijkstra algorithm
+*/
 class vertex
 {
 
-	std::vector<std::pair<int,int>> edges;				//first int: edge destination, second int: edge weight
-	int prev;
-	long long  dist;
-	bool used;
+	std::vector<std::pair<int,int>> edges;				///< first int: edge destination, second int: edge weight
+	int prev;							///< the previous vertex in the path			
+	long long  dist;						///< the distance from the vertex 1
+	bool used;							///< label if the vertex is already used in the dijkstra algorithm
 	
 
 public:
