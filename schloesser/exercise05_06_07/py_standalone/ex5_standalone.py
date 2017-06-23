08a30159834e6cf94eb66871b34a7689514f2201
Franziska Schlösser <schloesser@zib.de>
2017-06-23 18:11:33 +0200

    fix py_standalone

--- schloesser/exercise05_06_07/py_standalone/ex5_standalone.py
+++ schloesser/exercise05_06_07/py_standalone/ex5_standalone.py
@@ -1,9 +1,33 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 
+'''
+Exercises 5, 6 and 7
+
+This package provides means to solving exercises 5 to 7 from the lecture appfs.
+'''
 import sys
 import time
 
+def pop_next_destination(to_visit, distance):
+    '''
+    Return a vertex that still has to be visited and has currently smallest distance to startvertex.
+    '''
+    mind = -1
+    minv = None
+    for vert in to_visit:
+        # assert distance[v] >= 0
+        if distance[vert] < mind or mind == -1:
+            mind = distance[vert]
+            minv = vert
+    to_visit.remove(minv)
+    return minv
+
 class Vertex:
+    '''
+    Vertex
+
+    This class holds all relevant information about a vertex.
+    '''
 
     def __init__(self, name):
         self.name = name
@@ -12,7 +36,7 @@ class Vertex:
 
     def __str__(self):
         return "Vert({})".format(self.name)
-    
+
     def __repr__(self):
         return self.__str__()
 
@@ -21,29 +45,49 @@ class Vertex:
 
     def __gt__(self, other):
         return self.name > other.name
-    
+
     def __ge__(self, other):
         return self.name >= other.name
-    
+
     def __le__(self, other):
         return self.name <= other.name
 
     def add_incoming_edge(self, edge):
+        '''
+        Add an incoming edge.
+        '''
         self.edges_in.append(edge)
 
     def add_outgoing_edge(self, edge):
+        '''
+        Add an outgoing adge.
+        '''
         self.edges_out.append(edge)
-    
+
     def get_incoming_edges(self):
+        '''
+        Return list of incoming edges.
+        '''
         return self.edges_in
 
     def get_outgoing_edges(self):
+        '''
+        Return list of outgoing edges.
+        '''
         return self.edges_out
 
     def get_name(self):
+        '''
+        Return name of Vertex
+        '''
         return self.name
-    
+
 class Edge:
+    '''
+    Edge
+
+    This class holds all relevant information about an edge.
+    '''
 
     # source and target are Vertices, w is int
     def __init__(self, source, target, w):
@@ -52,6 +96,9 @@ class Edge:
         self.target = target
 
     def register(self):
+        '''
+        Register edge to source and target vertices.
+        '''
         self.source.add_outgoing_edge(self)
         self.target.add_incoming_edge(self)
 
@@ -60,98 +107,159 @@ class Edge:
 
     def __repr__(self):
         return self.__str__()
-    
+
     def get_source(self):
+        '''
+        Return source of edge.
+        '''
         return self.source
 
     def get_target(self):
+        '''
+        Return target of edge.
+        '''
         return self.target
 
     def get_weight(self):
+        '''
+        Return weight of edge.
+        '''
         return self.weight
 
 class Graph:
+    '''
+    Graph
+
+    This lass holds all relevant information about a graph.
+    '''
 
     def __init__(self, filename, directed=False):
+        self.vertices = []
+        self.edges = []
+        self.n_vertices = 0
+        self.n_edges = 0
         self.init_from_file(filename, directed)
 
     def __str__(self):
-        return "Graph\nVertices: {}\nEdges: {}".format(self.vertices, "\n".join(map(lambda e: "\t"+e.__str__(), self.edges)))
+        return "Graph\nVertices: {}\nEdges: {}".format(
+            self.vertices, "\n".join(map(lambda e: "\t"+e.__str__(), self.edges)))
 
     def __repr__(self):
         return self.__str__()
-    
+
     def reset(self):
+        '''
+        Reset all information: vertices, edges, n_vertices, n_edges.
+        '''
         self.vertices = []
         self.edges = []
         self.n_vertices = 0
         self.n_edges = 0
 
-    def add_edge(self, v1, v2, w):
-        e = Edge(v1, v2, w)
-        e.register()
-        self.edges.append(e)
+    def add_edge(self, vert1, vert2, weight):
+        '''
+        Add an edge to graph.
+
+        Args:
+            vert1 (int): source vertex
+            vert2 (int): destination vertex
+            weight (int): weight of edge.
+        '''
+        edge = Edge(vert1, vert2, weight)
+        edge.register()
+        self.edges.append(edge)
         self.n_edges = self.n_edges + 1
 
-    def add_vertex(self, v): 
-        self.vertices.append(Vertex(v))
+    def add_vertex(self, vert):
+        '''
+        Add a vertex to graph.
+
+        Args:
+            vert (int): Vertex name.
+        '''
+        self.vertices.append(Vertex(vert))
         self.n_vertices = self.n_vertices + 1
 
     def init_from_file(self, filename, directed=False):
+        '''
+        Read in a file containing a graph.
+        First line contains two numbers: number of vertices, number of edges.
+        Then the (weighted) edges are specified linewise: source target weight.
+
+        Args:
+            filename (string): name of file
+            directed (boolean): default False for undirected graph, True for directed graph.
+        '''
         self.reset()
 
-        with open(filename, "r") as f:\
+        with open(filename, "r") as infile:
             # assume first line has number of vertices and number of edges
-            line = f.readline()
+            line = infile.readline()
             info = line.split(" ")
 
-            n_vert = int(info[0]) 
-            n_edges = int(info[1])
+            n_vert = int(info[0])
+            #n_edges = int(info[1])
 
             # assume file contains vertices in range 1 .. n_vertices
             for i in range(n_vert):
                 self.add_vertex(i+1)
-            
+
             # assume file contains lines describing edges in form "vertex1 vertex2 weight"
-            for i, line in enumerate(f):
+            for i, line in enumerate(infile):
                 info = line.split(" ")
-                
-                v1 = self.vertices[int(info[0])-1]
-                v2 = self.vertices[int(info[1])-1]
+
+                vert1 = self.vertices[int(info[0])-1]
+                vert2 = self.vertices[int(info[1])-1]
                 # assume w positive
-                w = int(info[2])
-                
-                self.add_edge(v1, v2, w)
+                weight = int(info[2])
+
+                self.add_edge(vert1, vert2, weight)
                 if not directed:
-                    self.add_edge(v2, v1, w)
+                    self.add_edge(vert2, vert1, weight)
 
     def get_vertex_by_name(self, vertex):
-        # vertex is a string
-        for v in self.vertices:
-            if v.get_name() == vertex:
-                return v
+        '''
+        Return the name associated to the vertex.
+
+        Args:
+            vertex (string): the name of the vertex.
+
+        Return:
+            int: index of vertex with name vertex
+        '''
+        for vert in self.vertices:
+            if vert.get_name() == vertex:
+                return vert
 
     def get_min_unvisited(self, visited, distance):
-        mind = -1
-        minv = None
-        for v in self.vertices:
-            if distance[v] != -1 and not visited[v] and (distance[v] < mind or mind == -1):
-                mind = distance[v]
-                minv = v
-        return minv
+        '''
+        Return an unvisited vertex with current shortest distance amongst all unvisited vertices.
 
-    def pop_next_destination(self, to_visit, distance):
+        Args:
+            visited (array): an index-based array containing whether or not a vertex has been visited.
+            distance (array): an index-based array containing the current shortest distances to a vertex.
+
+        Return:
+            int: index of nearest unvisited vertex.
+        '''
         mind = -1
         minv = None
-        for v in to_visit:
-            # assert distance[v] >= 0
-            if distance[v] < mind or mind == -1:
-                mind = distance[v]
-                minv = v
-        to_visit.remove(minv)
+        for vert in self.vertices:
+            if distance[vert] != -1 and not visited[vert] and (distance[vert] < mind or mind == -1):
+                mind = distance[vert]
+                minv = vert
         return minv
 
     def calculate_distances_to(self, vert):
+        '''
+        Calculate shortest distances to a vertex.
+
+        Args:
+            vert (int): Vertex to which the lengths of the shortest paths should be calculated.
+
+        Return:
+            array: containing shortest distance to each vertex.
+        '''
         distance = {}
         visited = {}
         for vertex in self.vertices:
@@ -159,48 +267,60 @@ class Graph:
             visited[vertex] = False
         distance[vert] = 0
         to_visit = set()
-        
+
         to_visit.add(vert)
-        while len(to_visit) != 0:
-            curr = self.pop_next_destination(to_visit, distance)
+        n_next = len(to_visit)
+        while n_next != 0:
+            curr = pop_next_destination(to_visit, distance)
             visited[curr] = True
             edges = curr.get_incoming_edges()
             for edge in edges:
                 neighbor = edge.get_source()
-                if not visited[neighbor]: 
+                if not visited[neighbor]:
                     # update distances
                     newdist = distance[curr] + edge.get_weight()
                     if distance[neighbor] == -1 or newdist < distance[neighbor]:
                         distance[neighbor] = newdist
                     to_visit.add(neighbor)
+            n_next = len(to_visit)
         return distance
-    
+
     def get_longest_shortest_to(self, vert):
+        '''
+        Return the length and start vertex of a shortest path to vert.
+        If more than one path hast shortest length, the one with the smallest index is returned.
+
+        Args:
+            vert (int): vertex to which a shortest path is being searched.
+
+        Return:
+            (int, int): pair of vertex and distance.
+        '''
         distances = self.calculate_distances_to(vert)
         max_dist = 0
         max_vert = vert
-        for v in sorted(self.vertices):
-            dist = distances[v]
+        for vertex in sorted(self.vertices):
+            dist = distances[vertex]
             if dist > max_dist:
                 max_dist = dist
-                max_vert = v
+                max_vert = vertex
         return (max_vert, max_dist)
 
 if __name__ == "__main__":
-    t0_clock = time.perf_counter()
-    t0_time = time.process_time()
-    
-	# read filename from commandlineargs
-    filename = sys.argv[1]
-    #vert = int(sys.argv[2])
-    vert = int(1)
-    g = Graph(filename, False)
+    T0CLOCK = time.perf_counter()
+    T0TIME = time.process_time()
 
-    vert = g.get_vertex_by_name(vert)
-    res = g.get_longest_shortest_to(vert)
-    
-    print("RESULT VERTEX {}\n\nRESULT DIST {}".format(res[0].get_name(), res[1]))
-    t1_clock = time.perf_counter()
-    t1_time = time.process_time()
-    print("Program took {} s (wallclock time)".format(t1_clock - t0_clock))
-    print("Program took {} s (process time)".format(t1_time - t0_time))
+    # read filename from commandlineargs
+    INFILE = sys.argv[1]
+    #vert = int(sys.argv[2])
+    VERT = int(1)
+    GRAPH = Graph(INFILE, False)
+
+    VERT = GRAPH.get_vertex_by_name(VERT)
+    RES = GRAPH.get_longest_shortest_to(VERT)
+
+    print("RESULT VERTEX {}\n\nRESULT DIST {}".format(RES[0].get_name(), RES[1]))
+    T1CLOCK = time.perf_counter()
+    T1TIME = time.process_time()
+    print("Program took {} s (wallclock time)".format(T1CLOCK - T0CLOCK))
+    print("Program took {} s (process time)".format(T1TIME - T0TIME))
