b818ea5d45f458a4d4e6bef128b68d0e09fc40b9
Tam <tktamtran@gmail.com>
2017-06-29 23:12:41 +0200

    ex7 submission with makefile etc

--- Tam/exercise7/ex7.py
+++ Tam/exercise7/ex7.py
@@ -5,6 +5,8 @@
 # goals:
 # 2 modes of calculating the longest shortest path, library vs self written
 
+""" This program will calculate the longest shortest path of a graph from the vertex 1 and it will use either the library-given method or the self-written method as specified """
+
 import sys
 from collections import defaultdict, OrderedDict
 from itertools import chain
@@ -14,13 +16,28 @@ import networkx as nx
 import operator
 
 def run(mode, graphfile):
+    """Executes the program on the given input arguments
+        
+        Args:
+            mode: indicate which method to execute program with
+            graphfile: the graph to traverse
+        
+        Returns: print statement indicating the furthest vertex from Vertex 1 and its respective distance
+        
+        Raises: ValueError and AssertionError
+    """
     
     if mode == '-m1': noLibrary(graphfile)
     elif mode == '-m2': yesLibrary(graphfile)
-    else: raise ValueError('please specify either \'-m1\' or \'-m2\'')
+    else: print('please specify either \'-m1\' or \'-m2\'')
 
 
 def yesLibrary(graphfile):
+    """Calculates the longest shortest path using the python package NetworkX. With respect to this program, this function is invoked in the run() function.
+        
+        Args:
+            graphfile: the graph to traverse
+    """
 
     # generate graph using only edges w/ weights
     G = nx.read_edgelist(graphfile, nodetype=int, data=(('weight',float),))
@@ -39,7 +56,12 @@ def yesLibrary(graphfile):
 
 
 def noLibrary(graphfile):
-
+    """Calculates the longest shortest path using self-written Djisktra's algorithm and dictionary data objects. With respect to this program, this function is invoked in the run() function.
+        
+        Args:
+            graphfile: the graph to traverse
+    """
+    
     # generate 'graph' dictionary
     d1, d2, graph = defaultdict(list), defaultdict(list), defaultdict(list)
     with open(graphfile) as f:
@@ -81,6 +103,18 @@ def noLibrary(graphfile):
 
 
 def update_distances(graph, distances, s, unvisited):
+    """Per iteration of evaluating the distances of paths from a single node, this function updates the data object keeping track of such distances. With respect to this program, this function is invoked in the noLibrary() function.
+        
+        Args:
+            graph: a dictionary data object containing information of the graph's vertices and edges
+            distances: a dictionary data object containing the current vertex distances from Vertex 1 and its preceeding vertix for the path from Vertex 1
+            s: an index for the current vertex being traversed
+            unvisited: a list of the vertices of graph that have not yet been traversed
+            
+        Returns:
+            distances: an updated data object
+            next_s: the next vertex to traverse
+    """
     
     s_neighbors = graph[s]
     for (dnode, edge_weight) in s_neighbors:
