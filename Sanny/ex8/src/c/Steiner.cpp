baafd20ae2e4fe58b99b07e2659864f69e7b28c2
Alessandro Bartsch <alebartsch@hotmail.com>
2017-07-06 20:09:32 +0200

    comments

--- Sanny/ex8/src/c/Steiner.cpp
+++ Sanny/ex8/src/c/Steiner.cpp
@@ -16,6 +16,7 @@ Steiner::~Steiner() {
 	delete dijsktra;
 }
 
+/** Computes all primes less than vertexCount */
 Primes* Steiner::getPrimes(unsigned int vertexCount) {
 	Primes* primes = new Primes();
 	primes->push_back(2);
@@ -33,15 +34,24 @@ Primes* Steiner::getPrimes(unsigned int vertexCount) {
 	return primes;
 }
 
+/** Uses prime numbers for terminals and solves the Steiner-tree problem */
 unsigned int Steiner::steiner(unsigned int vertexCount, Edges& edges, Weights& weights) {
 	if(vertexCount < 2){
 		return 0;
 	}
+	int weight = 0;
 	Primes* primes = getPrimes(vertexCount);
 
-	while(!primes->empty()){
 
+	// while there are primes not in the steiner-tree
+	while(!primes->empty()){
+		//go through all missing primes
+		for (int prime : *primes){
+
+		}
 	}
-	return 1;
+
+	delete primes;
+	return weight;
 }
 
