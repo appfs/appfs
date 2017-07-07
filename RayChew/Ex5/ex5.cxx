861c3f517af6b64f014fd25cc027cb0adabd8cc2
RayC <ray.chew@campus.tu-berlin.de>
2017-07-07 11:54:32 +0200

    fixed typo in ex5.cxx

--- RayChew/Ex5/ex5.cxx
+++ RayChew/Ex5/ex5.cxx
@@ -100,7 +100,7 @@ int main(int argc, char*argv[]){
     }
     // if distance == maxDistance, check if vertex index is smaller.
     if ((distances[*vertexPair.first] == maxDistance) && (*vertexPair.first < maxVertex)){
-      maxDistance = distances[*vertexPair.first];
+      maxVertex = *vertexPair.first;
     }
   }
   
