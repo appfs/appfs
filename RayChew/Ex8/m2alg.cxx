9353feee5d5512470b3762b1cb8a08ae02f421b1
RayC <ray.chew@campus.tu-berlin.de>
2017-07-14 00:13:16 +0200

    ex8 2nd push. todo: add comments, add heap datat structure

---
+++ RayChew/Ex8/m2alg.cxx
@@ -0,0 +1,79 @@
+#include "m2alg.h"
+
+/* Boost Bimap Container includes for own algorithm. */
+#include <boost/bimap.hpp>
+#include <boost/bimap/multiset_of.hpp>
+#include <boost/bimap/support/lambda.hpp> /* for bimap::_data and bimap::_key */
+
+namespace bm = boost::bimaps;
+using bimap = bm::bimap<int, boost::bimaps::multiset_of<int,std::less<int>>>;
+
+bimap m2alg::build_unvisited(int& n, int& startTerminal, std::vector<std::pair<int,int>>& edges) {
+  /* start initializing unvisited set */
+  bimap Unvisited; /// define unvisited set as a boost::bimap container.
+  std::vector<int> nodes = utils::get_Nodes(edges);
+  for(int i=1; i<n; i++) {
+    Unvisited.left.insert(bimap::left_value_type(i, std::numeric_limits<int>::max()));
+  }
+  
+  bimap::left_iterator itr = Unvisited.left.find(startTerminal);
+  Unvisited.left.replace_data(itr, 0);
+  
+  if (!nodes.empty()) {
+    for(auto j=nodes.begin(), end=nodes.end(); j!=end; j++) {
+      bimap::left_iterator itr = Unvisited.left.find(*j);
+      Unvisited.left.replace_data(itr, 0);    
+    }
+  }
+
+  return Unvisited;
+  /* end initializing unvisited set */
+}
+
+std::pair<std::vector<int>,std::vector<int>> m2alg::m2Algorithm (int& n, bimap& Unvisited, std::vector<std::vector<std::pair<int,int>>>& adjList, int& startTerminal) {
+  std::vector<int> finalWeights(n, std::numeric_limits<int>::max());
+  std::vector<int> finalParents(n);
+
+  finalWeights[startTerminal] = 0;
+  finalParents[startTerminal] = startTerminal;
+  
+  /* start calculating and updating distances */
+  //std::cout << "calculating distances..." << std::endl;
+  while(Unvisited.size()>0) {
+    auto minPair = Unvisited.right.begin();
+    int minIdx = minPair->second;
+    int minDist = minPair->first;
+    
+    signed int adjListSize = adjList[minIdx].size();
+    
+    for(int j=0; j<adjListSize; j++) {
+      int neighbour = adjList[minIdx][j].first;
+      int dist = adjList[minIdx][j].second;
+      int newDist = minDist + dist;
+      int nPWeight = finalWeights[neighbour];
+      
+      if (newDist < nPWeight) {
+	auto toBeReplaced = Unvisited.left.find(neighbour);
+	if (toBeReplaced != Unvisited.left.end()){
+	  Unvisited.left.modify_data(toBeReplaced, bm::_data=newDist);
+	}
+	finalWeights[neighbour] = newDist;
+	finalParents[neighbour] = minIdx;
+      }
+    }
+    Unvisited.left.erase(minIdx);
+  }
+  return std::make_pair(finalWeights,finalParents);
+  /* end calculating and updating distances */
+}
+
+int m2alg::get_NeighbourIndex(int& currentNode, int& neighbour, std::vector<std::vector<vertex>>& adjList) {
+  int j = 0;
+  for (auto i=adjList[currentNode].begin(), end=adjList[currentNode].end(); i!=end; i++) {
+    if (i->first==neighbour) {
+      break;
+    }
+    j++;
+  }
+  return j;
+}
---
+++ RayChew/Ex8/utils.h
@@ -0,0 +1,41 @@
+/* -- Includes -- */
+/* C++ includes. */
+#include <iostream> /* for std::cout, std::ofstream */
+#include <fstream> /* for fstream::app */
+#include <utility> /* for std::pair */
+#include <vector> /* for std::vector */
+#include <iterator> /* for std::distance */
+#include <set> /* for std::set */
+
+/* Boost qi parser and timer includes. */
+#include <boost/spirit/include/qi.hpp>
+#include <boost/timer/timer.hpp>
+
+using namespace boost::spirit;
+using qi::int_;
+using qi::double_;
+using qi::parse;
+
+#ifndef UTILS_H
+#define UTILS_H
+
+using Edge = std::pair<int, int>; // vector to store std::pair of two vertex indices.
+using vertex = std::pair<int,int>; // vector to store std::pair of vertex index and weight.
+
+namespace utils {
+  std::vector<int> get_Nodes(std::vector<Edge>& edges);
+  
+  std::pair<std::vector<Edge>,std::vector<int>> get_EdgesWeights (int& n, std::ifstream& file);
+  
+  std::vector<int> gen_Primes(int& n);
+
+  std::vector<int> get_PrimeDists(std::vector<int>& dists, std::vector<int>& primes);
+  
+  std::pair<int,int> get_Min(std::vector<int>& primeDists, std::vector<int>& primes);
+  
+  std::vector<std::vector<vertex>> build_adjList(int& n, std::vector<Edge>& edges, std::vector<int>& weights);
+
+  std::vector<std::vector<int>> build_adjListNoWeights(int& n,std::vector<Edge>& edges);
+};
+
+#endif
