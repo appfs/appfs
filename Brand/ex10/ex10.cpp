b3a2e5e1d8b259553bf3f5d7fc7f0860992fe5f9
micbra89 <michael.brand89@gmx.de>
2017-07-20 14:12:09 +0200

    Updated ex10 and makefile
    ex10
    - added tree test
    makefile
    - make creates file ex10
    - "make THREADS=X FILE=Y test" runs ex10 with X threads on file Y with source nodes that are set in makefile as variable

--- Brand/ex10/ex10.cpp
+++ Brand/ex10/ex10.cpp
@@ -126,7 +126,7 @@ int dijkstra_mod(const Graph &graph, Graph &steinerGraph, vertex_ root) {
     pq.erase(it);
     
     if((u != root) && (isPrime(u+1))){
-      for(int chk=0; chk<remPrimes.size(); chk++){
+      for(unsigned int chk=0; chk<remPrimes.size(); chk++){
         if((remPrimes[chk])==(u+1)){
           //NEW PRIME FOUND
           pFound = true;
@@ -138,7 +138,7 @@ int dijkstra_mod(const Graph &graph, Graph &steinerGraph, vertex_ root) {
           int lV = u;
           do{
             int preV = pre[lV];
-            for(int eIndx = 0; eIndx < graph[lV].size(); eIndx++){
+            for(unsigned int eIndx = 0; eIndx < graph[lV].size(); eIndx++){
               if(graph[lV][eIndx].first==preV){
                 steinerGraph[lV].push_back(Edge(preV, graph[lV][eIndx].second));
                 steinerGraph[preV].push_back(Edge(lV, graph[lV][eIndx].second));
@@ -171,6 +171,79 @@ int dijkstra_mod(const Graph &graph, Graph &steinerGraph, vertex_ root) {
   return wghtLocTree;
 }
 
+/*! \fn int validateStGraph(&gphToChk)
+*   \brief tests if gphToChk is connected and inherits all primes
+*   \param &grphToChk minimal SteinerTree found
+*   \return 1 if successful, 0 if unsuccessful
+*/
+int validateStGraph(Graph &gphToChk){
+
+	std::vector<int> remPrimes = getRequiredPrimes(gphToChk.size());
+	int totPrimes = remPrimes.size();
+	int stTreePrimeCount = 0;
+
+	bool visited[gphToChk.size()];
+	for(int i=0; i<gphToChk.size(); i++)
+		visited[i]=false;
+
+	
+
+	//start at vertex 2
+	if(!(gphToChk[1].empty())){
+
+		set< Edge , pq_compare> pq;
+		set< Edge , pq_compare > ::iterator it;
+
+		int u,v,wt;
+
+		pq.insert(Edge(1,0));
+
+		while(pq.size() != 0){
+	    it = pq.begin();
+		  u = it->first;
+		  pq.erase(it);
+		  if(visited[u]){
+		  	//fprintf(stderr, "vertex %i already visited.\n", u+1);
+		  	return 0;
+		  } 
+			visited[u] = true;
+			for(vector<Edge>::const_iterator ni = gphToChk[u].begin(); ni != gphToChk[u].end(); ni++){
+				v  = ni->first;
+	     	wt = ni->second;
+	     	//fprintf(stdout, "Insert edge (%i,%i)\n", u+1,v+1);
+	     	pq.insert(Edge(v,wt));
+	     	//Delete ṕarrallel edge from minSteinerGraph
+	     	for(int k=0; k<gphToChk[v].size(); k++){
+	     		if(gphToChk[v][k].first == u){
+	     			//fprintf(stdout, "Delete edge (%i,%i) from Graph\n", v+1,u+1);
+	     			gphToChk[v].erase(gphToChk[v].begin()+k);
+	     		}
+	     	}
+			}
+    }
+
+    //fprintf(stderr, "stGraph is Tree.\n");
+
+    for(int j=1; j<gphToChk.size(); j++){
+    	if(isPrime(j-1)){
+    		if(!(gphToChk[j-1].empty()))
+    			stTreePrimeCount++;
+    	}
+    }
+
+    if(stTreePrimeCount = totPrimes){
+    	//fprintf(stderr, "stGraph inherits all primes.\n");
+    	return 1;
+    }
+    else{
+    	//fprintf(stderr, "too little prime no.\n");
+    	return 0;
+    }
+  }
+  else{
+		return 0;
+	}
+}
 
 
 int main (int argc, char* argv[]) {
@@ -178,14 +251,17 @@ int main (int argc, char* argv[]) {
   * start timers for cpu and wall time
   */
   clock_t cpu0 = clock();
-  auto   wall0 = chrono::system_clock::now();
+  bool output = false;
 
   /*
   *   Check input arguments
   */
-  if( argc < 4){
-      fprintf(stderr, "Call the program as: %s 'filename.gph' 'no_threads' 'n_root_nodes'\n", argv[0]);
+  if( argc < 3){
+      fprintf(stderr, "Call the program as: %s 'filename.gph' 'n_root_nodes' (-s)\n", argv[0]);
       exit(EXIT_FAILURE);
+  }
+  if(std::string(argv[argc-1]) == "-s"){
+  	output=true;
   }
 
   ifstream    file(argv[1]);
@@ -197,27 +273,22 @@ int main (int argc, char* argv[]) {
   }
 
   /*!
-  * read in no_threads
-  */
-  int no_threads = 1;
-  if(stoi(argv[2]) > 0){
-    no_threads = stoi(argv[2]);
-  }
-  else{
-    fprintf(stderr, "Invalid no_threads!\n");
-    exit(EXIT_FAILURE);
-  }
-  
-  /*!
   * Check input nodes and save in array
   */
+  if(output) argc--;
   std::vector<int> sourceVert;
-  for(int i=3; i<argc; i++){
-    if(stoi(argv[i]) > 0){
-      sourceVert.push_back(stoi(argv[i])-1);
-    }
-    else{
-      fprintf(stderr, "Invalid source node. Please make sure that root node indices match ((>=1) && <=graph.size())\n");
+  for(int i=2; i<argc; i++){
+  	try{
+  		if(stoi(argv[i]) > 0){
+      	sourceVert.push_back(stoi(argv[i])-1);
+   		}
+    	else{
+      	fprintf(stderr, "Invalid source node. Please make sure that root node indices match ((>=1) && <=graph.size())\n");
+      	exit(EXIT_FAILURE);
+    	}
+  	}
+    catch(...){
+    	fprintf(stderr, "Call the program as: %s 'filename.gph' 'n_root_nodes' (-s)\n", argv[0]);
       exit(EXIT_FAILURE);
     }
 
@@ -254,39 +325,38 @@ int main (int argc, char* argv[]) {
       */
       graph[stoi(vertex1)-1].push_back(Edge(stoi(vertex2)-1, stoi(weight)));
       graph[stoi(vertex2)-1].push_back(Edge(stoi(vertex1)-1, stoi(weight)));
-    }catch (invalid_argument& ia){
-      //when data is not a digit,
-      //std::stoi throws an invalid argument exception
-    } catch ( ... ){}
+    }catch (...){
+    	fprintf(stderr, "Corrupt data in .gph file.");
+      exit(EXIT_FAILURE);
+    }
 
   }
   file.close();
+
+  int minStVal = INT_MAX;
+  Graph minSteiner(numV);
+  //int minSrcNode;
+
+  auto   wall0 = chrono::system_clock::now(); 
   
   int locStTrWght[sourceVert.size()];
   
-  omp_set_num_threads(no_threads);  /*!< set no of threads according to input param */
-  #pragma omp parallel for shared(locStTrWght)
-  for(int initroot=0; initroot<sourceVert.size(); initroot++){
+  #pragma omp parallel for shared(locStTrWght, minStVal, minSteiner)
+  for(unsigned int initroot=0; initroot<sourceVert.size(); initroot++){
     Graph stGraph(numV);
     if(sourceVert[initroot]>=graph.size()){
       fprintf(stderr, "Invalid source node: %i. Please make sure that root node indices are lower than graph size!\n", sourceVert[initroot]+1);
       exit(EXIT_FAILURE);
     }
     locStTrWght[initroot] = dijkstra_mod(graph, stGraph, sourceVert[initroot]);
-    int stVert = 0;
-    for(int j=0; j<stGraph.size(); j++){
-      if(!(stGraph[j].empty())){
-        stVert++;
-      }
+    
+    if(locStTrWght[initroot]<minStVal){
+    	minSteiner = stGraph;
+    	minStVal = locStTrWght[initroot];
+    	//minSrcNode = sourceVert[initroot]+1;
     }
   }
-  
-  int chWght = locStTrWght[0];
-  for(int i=0; i<sourceVert.size(); i++){
-    if(locStTrWght[i]<chWght){
-      chWght = locStTrWght[i];
-    }
-  }
+
   double cpuTime = (clock() - cpu0) / (double) CLOCKS_PER_SEC;
   chrono::duration<double> wallDur = (chrono::system_clock::now() - wall0);
   double wallTime = wallDur.count();
@@ -294,11 +364,32 @@ int main (int argc, char* argv[]) {
   /*
   *   OUTPUT
   */
-  fprintf(stdout, "THRDS:\t %i\n", no_threads);
-  fprintf(stdout, "TLEN:\t %i\n", chWght);
+  int ocount=0;
+  fprintf(stdout, "TLEN:\t %i\n", minStVal);
+  if(output){
+  	fprintf(stdout, "TREE:\t");
+  	for(unsigned int i=0; i<minSteiner.size(); i++){
+  		if(!(minSteiner[i].empty())){
+  			for(unsigned int j=0; j<minSteiner[i].size(); j++){
+  				fprintf(stdout, "(%i,%i), ", i+1, minSteiner[i][j].first+1);
+  				ocount++;
+  				if((ocount%10)==0) fprintf(stdout, "\n\t");
+  			}
+  		}
+  	}
+  	fprintf(stdout, "\n");
+  }
   fprintf(stdout, "TIME:\t %f\n", cpuTime);
   fprintf(stdout, "WALL:\t %f\n", wallTime);
-  return 0;
 
+  /*!
+  *		RUN TEST
+  *		returns 1 if successful, 0 if unsuccessful
+  */
+  int validate = validateStGraph(minSteiner);
+
+  if(validate>0) fprintf(stdout, "Test successful\n");
+  else fprintf(stdout, "Test unsuccessful\n");
+  return 0;
 }
 
